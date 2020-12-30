d086da19f847f777f78045030d79f55d7c9cf0b4
Anton Van Assche <vanasscheanton@gmail.com>
2020-12-30 18:06:54 +0100

    Pihole admin page port change to 70

--- pihole.sh
+++ pihole.sh
@@ -5,3 +5,10 @@ wget https://install.pi-hole.net -P rpi-nas/pihole/index.html
 
 # Execute installer
 source ./rpi-nas/pihole/index.html
+
+sudo mv rpi-nas/config_files/lighttpd.conf /etc/lighttpd/lighttpd.conf
+sudo chmod 644 /etc/lighttpd/lighttpd.conf
+sudo chown root:root /etc/lighttpd/lighttpd.conf
+sudo sudo service lighttpd restart
+
+echo -e "[i] ${cyan}You are now able to access the pi-hole administrative interface at http://192.168.0.100:70/admin ${reset}"
