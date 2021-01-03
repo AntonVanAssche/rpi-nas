9f5ac159e612db3f566a6fa3dffc4ea2ad8625a9
Anton Van Assche <vanasscheanton@gmail.com>
2021-01-03 15:43:21 +0100

    Permissions & version update prometheus

--- grafana.sh
+++ grafana.sh
@@ -11,12 +11,14 @@ echo -e "[i] ${cyan}Updating system cache... ${reset}" ; sudo apt update &> /dev
 # Prometheus install
 echo
 echo -e "[i] ${cyan}Installing prometheus. ${reset}"
-	wget https://github.com/prometheus/prometheus/releases/download/$VERSION_PROMETHEUS/prometheus-$VERSION_PROMETHEUS.freebsd-armv7.tar.gz
+	wget https://github.com/prometheus/prometheus/releases/download/v$VERSION_PROMETHEUS/prometheus-$VERSION_PROMETHEUS.linux-armv7.tar.gz
 	tar xfz prometheus-$VERSION_PROMETHEUS.linux-armv7.tar.gz
 	rm -rf prometheus-$VERSION_PROMETHEUS.linux-armv7.tar.gz
-	mv prometheus-$VERSION_PROMETHEUS.linux-armv7/ /usr/sbin/prometheus/
+	sudp mv prometheus-$VERSION_PROMETHEUS.linux-armv7/ /usr/sbin/prometheus/
 	sudo cp -r rpi-nas/config_files/prometheus.service /etc/systemd/system/
 	sudo cp -r rpi-nas/config_files/prometheus.yml /usr/sbin/prometheus/
+    sudo chown -R root:root /usr/sbin/prometheus/
+    sudo chown -R root:root /etc/systemd/system/prometheus.service
 	sudo systemctl daemon-reload
 	sudo systemctl start prometheus
 	sudo systemctl status prometheus
@@ -28,8 +30,10 @@ echo -e "[i] ${cyan}Installing grafana. ${reset}"
 	wget https://dl.grafana.com/oss/release/grafana-$VERSION_GRAFANA.linux-armv7.tar.gz
 	tar xfz grafana-$VERSION_GRAFANA.linux-armv7.tar.gz
 	rm -rf grafana-$VERSION_GRAFANA.linux-armv7.tar.gz
-	mv grafana-$VERSION_GRAFANA/ /usr/sbin/grafana/
+	sudo mv grafana-$VERSION_GRAFANA/ /usr/sbin/grafana/
 	sudo cp -r rpi-nas/config_files/grafana.service /etc/systemd/system/
+    sudo chown -R root:root /usr/sbin/grafana/
+    sudo chown -R root:root /etc/systemd/system/grafana.service
 	sudo systemctl daemon-reload
 	sudo systemctl start grafana
 	sudo systemctl status grafana
@@ -40,9 +44,11 @@ echo
 echo -e "[i] ${cyan}Installing node exporter. ${reset}"
 	wget https://github.com/prometheus/node_exporter/releases/download/$VERSION_NODE_EPORTER/node_exporter-$VERSION_NODE_EPORTER.linux-armv7.tar.gz
 	tar xfz node_exporter-$VERSION_NODE_EPORTER.linux-armv7.tar.gz
-	mv node_exporter-$VERSION_NODE_EPORTER.linux-armv7/ /usr/sbin/node_exporter/
 	rm -rf node_exporter-$VERSION_NODE_EPORTER.linux-armv7.tar.gz
+    sudp mv node_exporter-$VERSION_NODE_EPORTER.linux-armv7/ /usr/sbin/node_exporter/
 	sudo cp -r rpi-nas/config_files/node_exporter.service /etc/systemd/system/
+    sudo chown -R root:root /usr/sbin/node_exporter/
+    sudo chown -R root:root /etc/systemd/system/node_exporter.service
 	sudo systemctl daemon-reload
 	sudo systemctl start node_exporter.service
 	sudo systemctl status node_exporter.service
@@ -50,3 +56,4 @@ echo -e "[i] ${cyan}Installing node exporter. ${reset}"
 	sudo cp -rf rpi-nas/config_files/prometheus.yml /usr/sbin/prometheus/
 
 sudo systemctl daemon-reload
+sudo systemctl restart grafana.service prometheus.service node_exporter.service
