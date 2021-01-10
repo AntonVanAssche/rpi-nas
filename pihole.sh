511b2def604e7d964009afe6121d3f33836c54b9
Anton Van Assche <vanasscheanton@gmail.com>
2021-01-10 12:36:04 +0100

    wget >> dir

--- pihole.sh
+++ pihole.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download pihole installer into rpi-nas/pihole/index.html
-wget https://install.pi-hole.net -P rpi-nas/pihole/index.html
+wget https://install.pi-hole.net -P rpi-nas/pihole/
 
 # Execute installer
 source ./rpi-nas/pihole/index.html
