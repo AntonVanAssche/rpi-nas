c86fa9b869abd21a36a8fd1dcfc6a2c7b624bfd3
Anton Van Assche <vanasscheanton@gmail.com>
2021-01-15 21:19:33 +0100

    mnt-nas.mount location

--- raid.sh
+++ raid.sh
@@ -26,7 +26,7 @@ echo -e "[i] ${cyan}Setting up raid...${reset}"
     sudo mount /dev/md0 /mnt/nas/
 
     # Copying mount service file >> /etc/systemd/system/
-    sudo cp -r config_files/mnt-nas.mount /etc/systemd/system/
+    sudo cp -r rpi-nas/config_files/mnt-nas.mount /etc/systemd/system/
     sudo chown root:root /etc/systemd/system/mnt-nas.mount
 
 # Set permissions
