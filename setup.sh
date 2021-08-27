ece626808c2138c9aa3aff1e993f7aad85e9345d
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-27 19:11:23 +0200

    Removed the needed input for the amount of the times you are running the script.

--- setup.sh
+++ setup.sh
@@ -14,37 +14,31 @@ read -t10
 
 ASK_FOR_SUDO
 
-message quest "Is this the first or second time you run the script? [first/second]"
-read -p " " RUN_TIME
-   if [[ $RUN_TIME = "first" ]] ; then
-      
-	  #Install NECESSARY packages
-	  source ./rpi-nas/packages.sh
+if [ ! -d "$ROCK_PI_CHECK" ]; then
 
-   elif [[ $RUN_TIME = "second" ]] ; then
+   #Install NECESSARY packages
+   source ./rpi-nas/packages.sh
 
-	  # Static ip setup
-	  source ./rpi-nas/ip.sh
+elif [ -d "$ROCK_PI_CHECK" ]; then
 
-	  # RAID 5 configuration
-	  source ./rpi-nas/raid.sh
+   # Static ip setup
+   source ./rpi-nas/ip.sh
+   
+   # RAID 5 configuration
+   source ./rpi-nas/raid.sh
 
-	  # Samba share setup
-	  source ./rpi-nas/samba.sh
+   # Samba share setup
+   source ./rpi-nas/samba.sh
 
-	  # Grafana setup (system monitoring)
-	  source ./rpi-nas/grafana.sh
+   # Grafana setup (system monitoring)
+   source ./rpi-nas/grafana.sh
 
-	  # Pihole setup
-	  source ./rpi-nas/pihole.sh
+   # Pihole setup
+   source ./rpi-nas/pihole.sh
 
-	  # Pivpn setup
-	  source ./rpi-nas/pivpn.sh
+   # Pivpn setup
+   source ./rpi-nas/pivpn.sh
 
-	else
-		message error "Unknown option: $RUN_TIME"
-		exit 22
-
-	fi
+fi
 
 REBOOT
