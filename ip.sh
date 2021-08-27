37fb8c8f6ca07caff549ad69db6337883172bc69
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-27 15:49:30 +0200

    Firewall

--- ip.sh
+++ ip.sh
@@ -18,3 +18,26 @@ static domain_name_servers=1.1.1.1 1.0.0.1
 
 message info "Restarting dhcpcd..."
 sudo systemctl restart dhcpcd
+
+message info "Setting up firewall..."
+sudo apt install ufw -y
+sudo ufw allow 22/tcp
+sudo ufw allow 53/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 139/tcp
+sudo ufw allow 445/tcp
+sudo ufw allow 1194/tcp
+sudo ufw allow 3000/tcp
+sudo ufw allow 8080/tcp
+sudo ufw allow 8096/tcp
+sudo ufw allow 9090/tcp
+sudo ufw allow 9100/tcp
+sudo ufw allow 51820/tcp
+
+sudo ufw allow 53/udp
+sudo ufw allow 137/udp
+sudo ufw allow 138/udp
+sudo ufw allow 1194/udp
+sudo ufw allow 51820/udp
+
+sudo ufw enable
