1faf44d094d212827d2ec520b276000103ed248b
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-30 17:32:35 +0200

    Added update-scripts/grafana-update.sh

---
+++ update-scripts/grafana-update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PROMETHEUS_VERSION="2.29.1"
+NODE_EXPORTER_VERSION="1.2.2"
+
+# Abort if error.
+set -e
+
+sudo apt update && sudo apt upgrade -y
+
+wget https://github.com/prometheus/prometheus/releases/download/v"$PROMETHEUS_VERSION"/prometheus-"$PROMETHEUS_VERSION".linux-armv7.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v"$NODE_EXPORTER_VERSION"/node_exporter-"$NODE_EXPORTER_VERSION".linux-armv7.tar.gz
+
+tar -xvf prometheus-"$PROMETHEUS_VERSION".linux-armv7.tar.gz
+tar -xvf node_exporter-"$NODE_EXPORTER_VERSION".linux-armv7.tar.gz
+
+rm -rf prometheus-"$PROMETHEUS_VERSION".linux-armv7.tar.gz node_exporter-"$NODE_EXPORTER_VERSION".linux-armv7.tar.gz
+
+sudo systemctl stop grafana-server prometeus.service node_exporter.service
+
+sudo rm -rf /usr/sbin/prometheus /usr/sbin/node_exporter
+
+sudo mv prometheus-"$PROMETHEUS_VERSION".linux-armv7/ prometheus/
+sudo mv node_exporter-"$NODE_EXPORTER_VERSION".linux-armv7/ node_exporter/
+
+sudo mv prometheus/ /usr/sbin/
+sudo mv node_exporter/ /usr/sbin/
+
+sudo cp -r ./configs/prometeus.yml /usr/sbin/prometheus/
+
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server prometheus.service node_exporter.service
+sudo systemctl status grafana-server prometheus.service node_exporter.service
+
+echo "All done"
